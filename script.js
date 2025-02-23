ad520a1c1e4b87014771acff4d25697b883e1e17
J-Coulson <167429496+J-Coulson@users.noreply.github.com>
2025-02-23 16:25:14 +0000

    Created computer choice function

---
+++ script.js
@@ -0,0 +1,15 @@
+//randomly returns one of the strings "rock", "paper" or "scissors"
+function getComputerChoice(){
+    const randomNumber = Math.floor((Math.random() * 3));
+    let choice = "";
+
+    if(randomNumber === 0){
+        choice = "rock";
+    } else if(randomNumber === 1){
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+
+    return(choice);
+}
