960946462e3862164a436a1f7fbaf569558a9e2d
J-Coulson <167429496+J-Coulson@users.noreply.github.com>
2025-02-23 17:53:42 +0000

    Created playGame function
    playRound and score now both local to playGame

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//global variables to keep score
-humanScore = 0;
-computerScore = 0;
+playGame();
 
 //randomly returns one of the strings "rock", "paper" or "scissors"
 function getComputerChoice(){
@@ -31,39 +29,60 @@ function getHumanChoice(){
     }
 }
 
-//Plays a single round of rock paper scissors
-function playRound(humanChoice, computerChoice){
-    if(humanChoice === "rock"){
-        if(computerChoice === "rock"){
-            console.log("Draw! Rock matches Rock");
-        } else if(computerChoice === "paper"){
-            console.log("You Lose. Paper wraps Rock");
-            computerScore += 1;
-        } else {
-            console.log("You Win! Rock blunts Scissors");
-            humanScore += 1;
-        }
-    } else if(humanChoice === "paper"){
-        if(computerChoice === "rock"){
-            console.log("You Win! Paper wraps Rock");
-            humanScore += 1;
-        } else if(computerChoice === "paper"){
-            console.log("Draw! Paper matches Paper");
-        } else {
-            console.log("You Lose! Scissors cut Paper");
-            computerScore += 1;
-        }
+//plays five rounds of rock paper scissors
+function playGame(){
+    //variables to keep game score
+    humanScore = 0;
+    computerScore = 0;
+
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+
+    if(humanScore > computerScore){
+        console.log("You Won! You won " + humanScore + 
+            " rounds, and the computer won " + computerScore + " rounds.");
+    } else if(humanScore < computerScore){
+        console.log("You Lost! You won " + humanScore + 
+            " rounds, and the computer won " + computerScore + " rounds.");
     } else {
-        if(computerChoice === "rock"){
-            console.log("You Lose! Rock blunts Scissors");
-            computerScore += 1;
-        } else if(computerChoice === "paper"){
-            console.log("You Win! Scissors cut Paper");
-            humanScore += 1;
+        console.log("Draw! You both won " + humanScore + " rounds.");
+    }
+
+    //Plays a single round of rock paper scissors
+    function playRound(humanChoice, computerChoice){
+        if(humanChoice === "rock"){
+            if(computerChoice === "rock"){
+                console.log("Draw! Rock matches Rock");
+            } else if(computerChoice === "paper"){
+                console.log("You Lose. Paper wraps Rock");
+                computerScore += 1;
+            } else {
+                console.log("You Win! Rock blunts Scissors");
+                humanScore += 1;
+            }
+        } else if(humanChoice === "paper"){
+            if(computerChoice === "rock"){
+                console.log("You Win! Paper wraps Rock");
+                humanScore += 1;
+            } else if(computerChoice === "paper"){
+                console.log("Draw! Paper matches Paper");
+            } else {
+                console.log("You Lose! Scissors cut Paper");
+                computerScore += 1;
+            }
         } else {
-            console.log("Draw! Scissors matches Scissors");
-        } 
+            if(computerChoice === "rock"){
+                console.log("You Lose! Rock blunts Scissors");
+                computerScore += 1;
+            } else if(computerChoice === "paper"){
+                console.log("You Win! Scissors cut Paper");
+                humanScore += 1;
+            } else {
+                console.log("Draw! Scissors matches Scissors");
+            } 
+        }
     }
 }
-
-playRound(getHumanChoice(), getComputerChoice());
