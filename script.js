d54ec0780bd0f4af9ed9280f6e78a0ea11cc05a2
J-Coulson <167429496+J-Coulson@users.noreply.github.com>
2025-03-28 16:56:24 +0000

    Added a basic ui to the rock paper scissors game
    ui is deliberately basic. designed for learning,
    not for quality

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-playGame();
+//variables to keep game score
+let humanScore = 0;
+let computerScore = 0;
+
+//play a round whenever a button gets clicked
+const inputPanel = document.querySelector("#inputPanel");
+inputPanel.addEventListener('click', (event) => {
+    const userChoice = event.target.id;
+
+    if(userChoice === "inputPanel"){
+        return;
+    }
+    playRound(userChoice, getComputerChoice());
+
+    const scoreDiv = document.querySelector("#score");
+    scoreDiv.textContent = `Player score: ${humanScore}\nComputer score: ${computerScore}`;
+
+    if(humanScore === 5 || computerScore === 5){
+        let gameOver = new CustomEvent("gameOver");
+        const finish = document.querySelector("#finish");
+        finish.dispatchEvent(gameOver);
+    }
+});
+
+//display final result when game finishes
+const finish = document.querySelector("#finish");
+finish.addEventListener('gameOver', (event) => {
+    if(humanScore === 5){
+        finish.textContent = "Game over, You win!";
+    }
+    else{
+        finish.textContent = "Game over, You lose!";
+    }
+});
+
+
+
+//---Function definitions---//
 
 //randomly returns one of the strings "rock", "paper" or "scissors"
 function getComputerChoice(){
@@ -16,73 +53,42 @@ function getComputerChoice(){
     return(choice);
 }
 
-//returns "rock", "paper", "scissors" or "Invalid input" based on user input
-function getHumanChoice(){
-    let userInput = prompt("Choose Rock, Paper or Scissors");
-    let lowerCaseInput = userInput.toLowerCase();
+//Plays a single round of rock paper scissors
+function playRound(humanChoice, computerChoice){
+    const choices = document.querySelector("#choices");
+    choices.textContent = `You chose ${humanChoice}, and the computer chose ${computerChoice}`
 
-    if( lowerCaseInput === "rock" || lowerCaseInput === "paper" || lowerCaseInput === "scissors"){
-        return(lowerCaseInput);
-    } else {
-        console.log("Invalid input, defaulting to rock.")
-        return("rock");
-    }
-}
+    const results = document.querySelector("#results");
 
-//plays five rounds of rock paper scissors
-function playGame(){
-    //variables to keep game score
-    humanScore = 0;
-    computerScore = 0;
-
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-
-    if(humanScore > computerScore){
-        console.log("You Won! You won " + humanScore + 
-            " rounds, and the computer won " + computerScore + " rounds.");
-    } else if(humanScore < computerScore){
-        console.log("You Lost! You won " + humanScore + 
-            " rounds, and the computer won " + computerScore + " rounds.");
-    } else {
-        console.log("Draw! You both won " + humanScore + " rounds.");
-    }
-
-    //Plays a single round of rock paper scissors
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice === "rock"){
-            if(computerChoice === "rock"){
-                console.log("Draw! Rock matches Rock");
-            } else if(computerChoice === "paper"){
-                console.log("You Lose. Paper wraps Rock");
-                computerScore += 1;
-            } else {
-                console.log("You Win! Rock blunts Scissors");
-                humanScore += 1;
-            }
-        } else if(humanChoice === "paper"){
-            if(computerChoice === "rock"){
-                console.log("You Win! Paper wraps Rock");
-                humanScore += 1;
-            } else if(computerChoice === "paper"){
-                console.log("Draw! Paper matches Paper");
-            } else {
-                console.log("You Lose! Scissors cut Paper");
-                computerScore += 1;
-            }
+    if(humanChoice === "rock"){
+        if(computerChoice === "rock"){
+            results.textContent = "Draw! Rock matches Rock";
+        } else if(computerChoice === "paper"){
+            results.textContent = "You Lose. Paper wraps Rock";
+            computerScore += 1;
         } else {
-            if(computerChoice === "rock"){
-                console.log("You Lose! Rock blunts Scissors");
-                computerScore += 1;
-            } else if(computerChoice === "paper"){
-                console.log("You Win! Scissors cut Paper");
-                humanScore += 1;
-            } else {
-                console.log("Draw! Scissors matches Scissors");
-            } 
+            results.textContent = "You Win! Rock blunts Scissors";
+            humanScore += 1;
         }
+    } else if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            results.textContent = "You Win! Paper wraps Rock";
+            humanScore += 1;
+        } else if(computerChoice === "paper"){
+            results.textContent = "Draw! Paper matches Paper";
+        } else {
+            results.textContent = "You Lose! Scissors cut Paper";
+            computerScore += 1;
+        }
+    } else {
+        if(computerChoice === "rock"){
+            results.textContent = "You Lose! Rock blunts Scissors";
+            computerScore += 1;
+        } else if(computerChoice === "paper"){
+            results.textContent = "You Win! Scissors cut Paper";
+            humanScore += 1;
+        } else {
+            results.textContent = "Draw! Scissors matches Scissors";
+        } 
     }
 }
